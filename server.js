30a8ebf1ea9548795725c0ad157764d4f75cbdba
swimninja247 <83324596+swimninja247@users.noreply.github.com>
2022-07-25 21:46:49 -0500

    Imported CORS

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
+const options = {
+    origin: 'http://localhost:8000',
+    useSuccessStatus: 200,
+}
+app.use(cors(options));
 
 app.get('/', (req, res) => {
     res.send("Welcome to the backend, bub. Poopoo.")
