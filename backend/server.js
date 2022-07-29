bbd8e8218b75b190819d01abd97a0e7659734c17
swimninja247 <83324596+swimninja247@users.noreply.github.com>
2022-07-28 20:19:00 -0500

    Added env variables, dynamic routing and controls

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
+
 const app = express();
+const { readdirSync } = require('fs');
 
 app.use(cors());
+
+readdirSync('./routes').map((route) => app.use('/', require('./routes/' + route)));
 
 app.get('/', (req, res) => {
     res.send("Welcome to the backend, bub. Poopoo.")
 });
 
-app.listen(8000, () => {
-    console.log('Server started...');
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}...`);
 });
