fc3f74a0bd7412ec98f40310de56507de4274a3f
swimninja247 <83324596+swimninja247@users.noreply.github.com>
2022-07-28 19:59:41 -0500

    Tracking frontend folder top

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,8 @@
 const express = require('express');
 const cors = require('cors');
-
 const app = express();
-const options = {
-    origin: 'http://localhost:8000',
-    useSuccessStatus: 200,
-}
-app.use(cors(options));
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send("Welcome to the backend, bub. Poopoo.")
