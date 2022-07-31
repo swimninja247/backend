4f7af82bf361e86d75c81bec9c364f74340cd3eb
swimninja247 <83324596+swimninja247@users.noreply.github.com>
2022-07-31 14:30:52 -0500

    Implemented more registration validation

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,5 +1,6 @@
-const { validateEmail } = require("../helpers/validation");
+const { validateEmail, validateLength } = require("../helpers/validation");
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 exports.register = async (req, res) => {
     try {
@@ -15,19 +16,48 @@ exports.register = async (req, res) => {
             gender,
         } = req.body;
 
+        // validate email format
         if (!validateEmail(email)) {
             return res.status(400).json({
                 message: "invalid email address",
             });
         }
 
-        return;
+        // make sure this user doesn't already exist
+        const check = await User.findOne({$or:[{email},{username}]});
+        if (check) {
+            return res.status(400).json({
+                message: "This user already exists."
+            })
+        }
+
+        if (!validateLength(first_name, 1, 30)) {
+            return res.status(400).json({
+                message: "invalid first name length",
+            });
+        }
+
+        if (!validateLength(last_name, 1, 30)) {
+            return res.status(400).json({
+                message: "invalid last name length",
+            });
+        }
+
+        if (!validateLength(password, 8, 30)) {
+            return res.status(400).json({
+                message: "invalid password length",
+            });
+        }
+
+        // encrypt password
+        const cryptedPassword = await bcrypt.hash(password, 12);
+
         const user = await new User({
             first_name,
             last_name,
             email,
             username,
-            password,
+            password: cryptedPassword,
             birthYear,
             birthMonth,
             birthDay,
--- backend/helpers/validation.js
+++ backend/helpers/validation.js
@@ -1,3 +1,6 @@
 exports.validateEmail = (email) => {
     return String(email).toLowerCase().match(/^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,12})(\.[a-z]{2,12})?$/);
-}
+};
+exports.validateLength = (text, min, max) => {
+    return text.length <= max && text.length >= min;
+};
